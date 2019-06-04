e0331d6d481ec7b4cb3e6df6d5557243e2795de4
msajatov <e1125768@student.tuwien.ac.at>
2019-06-04 16:37:40 +0200

    Stable version that can produce datacards from syst_full_sig for tt 2017

--- make_datacards.sh
+++ make_datacards.sh
@@ -2,14 +2,14 @@
 eval `scramv1 runtime -sh`
 cd ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017
 
-rm -r output/*_smhtt
+# rm -r output/*_smhtt
 
 
-MorphingSM2017 --input_folder_mt="Vienna/"  --input_folder_et="Vienna/"  --input_folder_tt="Vienna/"  --input_folder_em="KIT/"  --real_data=true --postfix="-ML" --embedding=true --classic_bbb=false  --auto_rebin=true   --stxs_signals=stxs_stage0 --categories=stxs_stage1  --era=2016  --output="2016_smhtt"
-MorphingSM2017 --input_folder_mt="Vienna/"  --input_folder_et="Vienna/"  --input_folder_tt="Vienna/"  --input_folder_em="KIT/"  --real_data=true --postfix="-ML" --embedding=true --classic_bbb=false  --auto_rebin=true   --stxs_signals=stxs_stage0 --categories=stxs_stage1  --era=2017  --output="2017_smhtt"
+# MorphingSM2017 --input_folder_mt="Test/"  --input_folder_et="Test/"  --input_folder_tt="Test/syst_full_sig/"  --input_folder_em="KIT/"  --channel="tt" --real_data=true --postfix="-m_vis" --embedding=true --classic_bbb=false  --auto_rebin=true   --stxs_signals=stxs_stage0 --categories=gof  --gof_category_name=tt_inclusive  --era=2016  --output="2016_smhtt"
+MorphingSM2017 --verbose="true"  --input_folder_mt="Test/"  --input_folder_et="Test/"  --input_folder_tt="Test/syst_full_sig"  --input_folder_em="KIT/"  --channel="tt"  --real_data=true --postfix="-m_vis" --embedding=true --classic_bbb=false  --auto_rebin=true   --stxs_signals=stxs_stage0 --categories=gof  --gof_category_name=tt_inclusive  --era=2017  --output="2017_smhtt"
 
 
-rm output/*_smhtt/*/125/*_em_*
+# rm output/*_smhtt/*/125/*_em_*
 
 # rm output/2016_smhtt/em/125/htt_*_??_*txt
 # rm output/2017_smhtt/em/125/htt_*_??_*txt
@@ -41,30 +41,30 @@ do
 done 
 
 
-mkdir -p output/cmb_smhtt/cmb/125
-mkdir -p output/cmb_smhtt/cmb/common
+# mkdir -p output/cmb_smhtt/cmb/125
+# mkdir -p output/cmb_smhtt/cmb/common
 
-cp -v -p output/2016_smhtt/cmb/125/*txt  output/cmb_smhtt/cmb/125/
-cp -v -p output/2016_smhtt/cmb/common/*  output/cmb_smhtt/cmb/common/
-cp -v -p output/2017_smhtt/cmb/125/*txt  output/cmb_smhtt/cmb/125/
-cp -v -p output/2017_smhtt/cmb/common/*  output/cmb_smhtt/cmb/common/
+# cp -v -p output/2016_smhtt/cmb/125/*txt  output/cmb_smhtt/cmb/125/
+# cp -v -p output/2016_smhtt/cmb/common/*  output/cmb_smhtt/cmb/common/
+# cp -v -p output/2017_smhtt/cmb/125/*txt  output/cmb_smhtt/cmb/125/
+# cp -v -p output/2017_smhtt/cmb/common/*  output/cmb_smhtt/cmb/common/
 
-mkdir -p output/2016_signal_smhtt/cmb/125
-mkdir -p output/2016_signal_smhtt/cmb/common
-mkdir -p output/2017_signal_smhtt/cmb/125
-mkdir -p output/2017_signal_smhtt/cmb/common
-mkdir -p output/cmb_signal_smhtt/cmb/125
-mkdir -p output/cmb_signal_smhtt/cmb/common
+# mkdir -p output/2016_signal_smhtt/cmb/125
+# mkdir -p output/2016_signal_smhtt/cmb/common
+# mkdir -p output/2017_signal_smhtt/cmb/125
+# mkdir -p output/2017_signal_smhtt/cmb/common
+# mkdir -p output/cmb_signal_smhtt/cmb/125
+# mkdir -p output/cmb_signal_smhtt/cmb/common
 
-cp -v -p output/2016_smhtt/cmb/125/*txt  output/2016_signal_smhtt/cmb/125/
-cp -v -p output/2016_smhtt/cmb/common/*  output/2016_signal_smhtt/cmb/common/
-cp -v -p output/2017_smhtt/cmb/125/*txt  output/2017_signal_smhtt/cmb/125/
-cp -v -p output/2017_smhtt/cmb/common/*  output/2017_signal_smhtt/cmb/common/
-cp -v -p output/cmb_smhtt/cmb/125/*txt  output/cmb_signal_smhtt/cmb/125/
-cp -v -p output/cmb_smhtt/cmb/common/*  output/cmb_signal_smhtt/cmb/common/
+# cp -v -p output/2016_smhtt/cmb/125/*txt  output/2016_signal_smhtt/cmb/125/
+# cp -v -p output/2016_smhtt/cmb/common/*  output/2016_signal_smhtt/cmb/common/
+# cp -v -p output/2017_smhtt/cmb/125/*txt  output/2017_signal_smhtt/cmb/125/
+# cp -v -p output/2017_smhtt/cmb/common/*  output/2017_signal_smhtt/cmb/common/
+# cp -v -p output/cmb_smhtt/cmb/125/*txt  output/cmb_signal_smhtt/cmb/125/
+# cp -v -p output/cmb_smhtt/cmb/common/*  output/cmb_signal_smhtt/cmb/common/
 
-rm output/2016_signal_smhtt/cmb/125/htt_*_??_*txt
-rm output/2017_signal_smhtt/cmb/125/htt_*_??_*txt
-rm output/cmb_signal_smhtt/cmb/125/htt_*_??_*txt
+# rm output/2016_signal_smhtt/cmb/125/htt_*_??_*txt
+# rm output/2017_signal_smhtt/cmb/125/htt_*_??_*txt
+# rm output/cmb_signal_smhtt/cmb/125/htt_*_??_*txt
 
 
