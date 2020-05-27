db3de2a19b7d75305c89325c9b485084d83a5588
msajatov <e1125768@student.tuwien.ac.at>
2020-05-27 18:45:26 +0200

    Modify result_crawler.sh to display all failed fits

--- MyCombineToolset/result_crawler.sh
+++ MyCombineToolset/result_crawler.sh
@@ -8,7 +8,7 @@ SHORTVARS="mt_2"
 
 #CONFIGS="cc cc1 cc2 nn1 nn2 nn3 nn4 nn5 nn6 nn7 nn8 nn9 nn10 nn11 nn12 nn13 nn14 nn15 nn16 nn17 nn18 nn21 nn1a nn2a nn3a nn4a nn5a nn6a nn7a nn8a nn9a nn10a nn11a nn12a nn13a nn14a nn15a nn16a nn17a nn18a nn22a nn23a xx w tt"
 #CONFIGS="cc cc1 cc2 nn1 nn2 nn3 nn4 nn5 nn6 nn7 nn8 nn9 nn10 nn11 nn12 nn13 nn14 nn15 nn16 nn17 nn18 nn21 nn22 nn23 xx w tt"
-CONFIGS="cc cc1 cc2"
+CONFIGS="snn47 snn48 snn49 snn50 snn51 snn52 snn53"
 
 MASS=125
 TOYS=25
@@ -30,7 +30,7 @@ for ERA in 2017 ; do
                 #display "htt_${CHANNEL}_100_Run2017_postfit.png"
                 echo "${CONF} --- ${VAR} --- channel ${CHANNEL}"
 
-                grep "Best fit r" fit_diagnostics.log
+                grep "Best fit r" grepoutput.log
                 
                 cd ${BASEDIR}
             done    			
